29b6225a2c1360acf5fd211458aa02e3874b4528
John Miller <tharealjohn@gmail.com>
2015-05-06 11:42:15 -0400

    Initial Samples

---
+++ NTLMExample/NTLMExample.Droid/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Org.Apache.Http.Impl.Client;
+using Org.Apache.Http.Authentication;
+using Org.Apache.Http.Client.Methods;
+using Org.Apache.Http.Entity;
+
+namespace NTLMExample.Droid
+{
+    [Activity(Label = "NTLMExample.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+			
+            button.Click += delegate
+            {
+                SendNTLMRequest();
+            };
+        }
+
+        void SendNTLMRequest()
+        {
+            DefaultHttpClient client = new DefaultHttpClient();
+
+            // Credentials are hardcoded here
+            NTCredentials credentials = new NTCredentials("username", "password", "workstation", "domain");
+            client.CredentialsProvider.SetCredentials(AuthScope.Any, credentials);
+
+            HttpPost post = new HttpPost("serviceurl");
+            var content = new StringEntity("requestcontent");
+            post.Entity = content;
+
+            var response = client.Execute(post);
+            int status = response.StatusLine.StatusCode;
+
+            // Check for communication errors
+            if (status != 200 && status != 500)
+            {          
+                Console.WriteLine("Response error {0}", status);
+            }
+            else
+            {
+                var responseContent = response.Entity;
+
+                // Do what you want with the content
+                var resultStream = responseContent.Content;
+            }
+        }
+    }
+}
+
+
---
+++ NTLMExample/NTLMExample.iOS/ViewController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+// Code in this example adopted from http://waynehartman.com/posts/how-to-do-ntlm-authentication-in-ios.html
+namespace NTLMExample.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            // Pass in your ULR instead of "someurl". 
+            NSUrlRequest request = new NSUrlRequest(NSUrl.FromString("someurl"));
+            NSUrlConnection connection = NSUrlConnection.FromRequest(request, new NativeUrlDelegate());
+
+        }            
+    }
+    // For a more complete Delegate implementation, take a look at this sample: https://github.com/xamarin/monotouch-samples/blob/master/CustomInputStream/NativeUploader.cs#L97
+    public class NativeUrlDelegate : NSUrlConnectionDataDelegate
+    {
+        public override void ReceivedAuthenticationChallenge(NSUrlConnection connection, NSUrlAuthenticationChallenge challenge)
+        {
+            if (challenge.ProtectionSpace.AuthenticationMethod == NSUrlProtectionSpace.AuthenticationMethodNTLM)
+            {
+                // Important to check! You could end up locking out your user based on your security polices after entering wrong credentials too many times!
+                if (challenge.PreviousFailureCount > 0)
+                {
+                    challenge.Sender.CancelAuthenticationChallenge(challenge);
+                    new UIAlertView("Invalid Credentials", "The credentials you saved for your account are invalid", null, "Ok").Show();
+                }
+                else
+                {
+                    // Credentials are hardcoded. Instead, you may want to show some UI to the user to enter them, etc.
+                    NSUrlCredential credentials = new NSUrlCredential("username", "password", NSUrlCredentialPersistence.ForSession);
+                    challenge.Sender.UseCredential(credentials, challenge);
+                }
+            }
+            else
+            {
+                // Do whatever you want when it's not NTLM. For this sample, we just cancel the challenge.
+                challenge.Sender.CancelAuthenticationChallenge(challenge);
+            }
+        }
+    }
+}
+
